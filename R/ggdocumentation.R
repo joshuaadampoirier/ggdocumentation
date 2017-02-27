f62760c477e221cc180dc8cf1e71e3c9c7c43387
joshuaadampoirier <josh.poirier@gmail.com>
2017-02-27 17:02:03 -0500

    supports most themes from ggthemes package, need generalize to support variants

--- R/ggdocumentation.R
+++ R/ggdocumentation.R
@@ -2,66 +2,202 @@ library(png)
 library(grid)
 library(ggplot2)
 
-getTheme <- function(theme="economist") {
+get_ggtheme <- function(theme="economist") {
+    # initialize list of 
+    t <- list()
     
     if (theme == "base") {
-        # a theme resembling the default base graphics in R    
-        cols <- c("white", "white", "black", TRUE, "black")
+
+        try ((if (!"package:ggthemes" %in% search()) 
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- "white"
+        t[["fg"]] <- "black"
+        t[["addrect"]] <- TRUE
+        t[["rectcol"]] <- "black"
+        t[["cex"]] <- .5
+        t[["theme"]] <- theme_base() + theme(rect=element_blank(), strip.background=element_rect(fill="white"))
     } else if (theme == "calc") {
-        # a theme based on LibreOffice Calc
-        cols <- c("white", "white", "black", TRUE, "black")
+        
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- "white"
+        t[["fg"]] <- "gray70"
+        t[["addrect"]] <- TRUE
+        t[["rectcol"]] <- "black"
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_calc() + theme(rect=element_blank(), strip.background=element_rect(color="black"))
+
     } else if (theme == "economist") {
-        # a theme based on the plots in The Economist newspaper
-        cols <- c("white", "grey", "black", TRUE, "black")
+        
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- ggthemes_data$economist$bg["ebg"]
+        t[["docbg"]] <- ggthemes_data$economist$bg["edkbg"]
+        t[["fg"]] <- ggthemes_data$economist$fg["blue_gray"]
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_economist()
+        
     } else if (theme == "economist_white") {
-        # ditto
-        cols <- c("white", "grey", "black", TRUE, "black")
+        
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- ggthemes_data$economist$bg["ltgray"]
+        t[["docbg"]] <- "white"
+        t[["fg"]] <- "black"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_economist_white()
+        
     } else if (theme == "excel") {
-        # a theme replicating classic charts in Excel
-        cols <- c("white", "grey", "black", TRUE, "black")
+        
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- "#C0C0C0"
+        t[["fg"]] <- "white"
+        t[["addrect"]] <- TRUE
+        t[["rectcol"]] <- "black"
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_excel()
+        
     } else if (theme == "few") {
-        # theme from Stephen Few's 'Practical Rules for Using Color in Charts'
-        cols <- c("white", "grey", "black", TRUE, "black")
+
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- "white"
+        t[["fg"]] <- "black"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_few()
+        
     } else if (theme == "fivethirtyeight") {
-        # theme based on the plots at fivethirtyeight.com
-        cols <- c("white", "grey", "black", TRUE, "black")
+        
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- ggthemes_data$fivethirtyeight["ltgray"]
+        t[["docbg"]] <- ggthemes_data$fivethirtyeight["dkgray"]
+        t[["fg"]] <- ggthemes_data$fivethirtyeight["ltgray"]
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_fivethirtyeight()
+        
     } else if (theme == "gdocs") {
-        # theme based on Google Docs
-        cols <- c("white", "grey", "black", TRUE, "black")
+
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- "white"
+        t[["fg"]] <- "#CCCCCC"
+        t[["addrect"]] <- TRUE
+        t[["rectcol"]] <- "black"
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_gdocs() + theme(rect=element_blank(), strip.background=element_rect(color="black"))
+        
     } else if (theme == "hc") {
-        # theme based on Highcharts JS
-        cols <- c("white", "grey", "black", TRUE, "black")
-    } else if (theme == "par") {
-        # theme that uses the current values of the base graphics parameters in par
-        cols <- c("white", "grey", "black", TRUE, "black")
+
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- ggthemes_data$hc$bg["darkunica"]
+        t[["fg"]] <- "white"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_hc() 
+        
     } else if (theme == "pander") {
-        # theme to use with the pander package
-        cols <- c("white", "grey", "black", TRUE, "black")
+        
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- "white"
+        t[["fg"]] <- "grey"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_pander() + theme(text=element_text(size=12))
+        
     } else if (theme == "solarized") {
-        # theme using the solarized color palette
-        cols <- c("white", "grey", "black", TRUE, "black")
+ 
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "#002b36"
+        t[["docbg"]] <- "grey85"
+        t[["fg"]] <- "#073642"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_solarized()
+        
     } else if (theme == "stata") {
-        # themes based on Stata graph schemes
-        cols <- c("white", "grey", "black", TRUE, "black")
+
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- ggthemes_data$stata$colors["ltbluishgray"]
+        t[["docbg"]] <- ggthemes_data$stata$colors["bluishgray"]
+        t[["fg"]] <- "black"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_stata()
+        
     } else if (theme == "tufte") {
-        # a minimal ink theme based on Tufte's The Visual Display of Quantitative Information
-        cols <- c("white", "grey", "black", TRUE, "black")
+
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- "white"
+        t[["fg"]] <- "black"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_tufte()
+        
     } else if (theme == "wsj") {
-        # a theme based on the plots from The Wall Street Journal
-        cols <- c("white", "grey", "black", TRUE, "black")
+
+        try ((if (!"package:ggthemes" %in% search())
+            stop("Error! ggthemes package not loaded.")))
+        
+        t[["bg"]] <- ggthemes_data$wsj$bg["brown"]
+        t[["docbg"]] <- ggthemes_data$wsj$bg["brown"]
+        t[["fg"]] <- "black"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- .75
+        t[["theme"]] <- theme_wsj()
+        
     }
     
-    names(cols) <- c("bg", "docbg", "fg", "addrect", "rectcol")
-    cols
+    t
 }
 
 doc_plot <- function(d, fname, aut="", aut_tit="", dsrc="", theme="") {
 
     if (theme != "") {
-        cols <- getTheme(theme)
+        t <- get_ggtheme(theme)
     } else {
-        cols <- c("white", "grey", "black", TRUE, "black")
-        names(cols) <- c("bg", "docbg", "fg", "addrect", "rectcol")
+        t <- list()
+        t[["bg"]] <- "white"
+        t[["docbg"]] <- "grey"
+        t[["fg"]] <- "black"
+        t[["addrect"]] <- FALSE
+        t[["cex"]] <- 1
+        t[["theme"]] <- theme(
+            rect=element_blank(), 
+            strip.background=element_rect(fill="grey"),
+            text=element_text(color="black", face="plain", size=16, vjust=.5, hjust=.5, lineheight=1)
+            )
     }
     
     # Set up the layout for grid 
@@ -72,27 +208,28 @@ doc_plot <- function(d, fname, aut="", aut_tit="", dsrc="", theme="") {
 
     # background color of plot
     pushViewport(viewport(layout.pos.row=1, layout.pos.col=1:7))
-    print(grid.draw(rectGrob(gp=gpar(fill=cols["bg"], col=ifelse(cols["addrect"], cols["rectcol"], cols["bg"])))), newpage=FALSE)
+    print(grid.draw(rectGrob(width=unit(.999, "npc"), 
+                             height=unit(.999, "npc"), 
+                             gp=gpar(fill=t$bg))), newpage=FALSE)
     popViewport()
     
     # background color of documentation
     pushViewport(viewport(layout.pos.row=2, layout.pos.col=1:7))
-    print(grid.draw(rectGrob(gp=gpar(fill=cols["docbg"], col=ifelse(cols["addrect"], cols["rectcol"], cols["docbg"])))), newpage=FALSE)
+    print(grid.draw(rectGrob(width=unit(.999, "npc"), 
+                             height=unit(.999, "npc"), 
+                             gp=gpar(fill=t$docbg))), newpage=FALSE)
     popViewport()
         
     # add plot to upper grid object
     pushViewport(viewport(layout.pos.row=1, layout.pos.col=1:7))
-    if (cols["addrect"]) {
-        #d$theme$rect[["colour"]] <- "white"
-        print(d$theme$rect[["colour"]])
-    }
+    d <- d + t$theme
     print(d, newpage=FALSE)
     popViewport()
     
     colWidths <- NULL
     for(i in 1:7) {
         pushViewport(viewport(layout.pos.row=1, layout.pos.col=i))
-        o <- rectGrob(gp=gpar(col="black", fill="#d5e4eb"))
+        o <- rectGrob()
         colWidths <- c(colWidths, widthDetails(o))
         popViewport()
     }
@@ -109,7 +246,7 @@ doc_plot <- function(d, fname, aut="", aut_tit="", dsrc="", theme="") {
         pushViewport(viewport(layout.pos.row=2, layout.pos.col=4))
         author_o <- textGrob(paste(aut, "\n", aut_tit), just=0,
                              x=unit(sum(colWidths[1:3])/sum(colWidths), "npc"), 
-                             gp=gpar(col=cols["fg"], cex=.8))
+                             gp=gpar(col=t$fg, fontfamily=t$theme$text$family, fontsize=t$cex*t$theme$text$size))
 
         print(grid.draw(author_o), newpage=FALSE)
         popViewport()        
@@ -120,10 +257,24 @@ doc_plot <- function(d, fname, aut="", aut_tit="", dsrc="", theme="") {
         pushViewport(viewport(layout.pos.row=2, layout.pos.col=6))
         source_o <- textGrob(paste("Source:", dsrc, "\n", format(Sys.Date(), "%d %b %Y")), just=1,
                              x=unit(sum(colWidths[1:6])/sum(colWidths), "npc"),
-                             gp=gpar(col=cols["fg"], cex=.8))
+                             gp=gpar(col=t$fg, fontfamily=t$theme$text$family, fontsize=t$cex*t$theme$text$size))
         print(grid.draw(source_o), newpage=FALSE)
         popViewport()        
     }
+    
+    # documentation border (if applicable)
+    pushViewport(viewport(layout.pos.row=2, layout.pos.col=1:7))
+    print(grid.draw(rectGrob(width=unit(.999, "npc"), 
+                             height=unit(.999, "npc"), 
+                             gp=gpar(fill=NA, col=ifelse(t$addrect, t$rectcol, t$docbg)))), newpage=FALSE)
+    popViewport()
+    
+    # plot border (if applicable)
+    pushViewport(viewport(layout.pos.row=1, layout.pos.col=1:7))
+    print(grid.draw(rectGrob(width=unit(.999, "npc"), 
+                             height=unit(.999, "npc"), 
+                             gp=gpar(fill=NA, col=ifelse(t$addrect, t$rectcol, t$docbg)))), newpage=FALSE)
+    popViewport()
     
     popViewport()
     
@@ -137,14 +288,15 @@ doc_plot <- function(d, fname, aut="", aut_tit="", dsrc="", theme="") {
 library(ggthemes)
 
 # Get the graph
-d <- ggplot(diamonds, aes(carat, price)) + theme_calc() +
+d <- ggplot(diamonds, aes(carat, price)) + theme_wsj() +
     xlim(0, 2) + 
     stat_binhex(na.rm = TRUE) + 
     theme(aspect.ratio = 1, legend.position="right") + 
     facet_wrap(~ color, scales = "free_x")
-doc_plot(d, 
-         fname=system.file("img", "Rlogo.png", package="png"), 
-         aut="Joshua Poirier", 
-         aut_tit="Data Scientist", 
-         dsrc="My website",
-         theme="calc")
+#d
+doc_plot(d,
+        fname=system.file("img", "Rlogo.png", package="png"),
+        aut="Joshua Poirier",
+        aut_tit="Data Scientist",
+        dsrc="My website",
+        theme="wsj")
